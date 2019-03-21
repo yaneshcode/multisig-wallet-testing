c33edeffd4ae04803c482d8a15a1ad69d7fb6506
yanesh <yaneshcode@gmail.com>
2019-03-20 21:16:51 -0400

    add ethers

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const MultiSigWallet = artifacts.require("MultiSigWallet");
 
 module.exports = function(deployer) {
-  deployer.deploy(MultiSigWallet);
+  deployer.deploy(MultiSigWallet(["0x0", "0x1"], 1));
 };
